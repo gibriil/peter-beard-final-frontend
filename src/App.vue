3657761be469fdc80c4f1411ad5bf50b5e613f16
Gibriil <musicmandanceman@yahoo.com>
2020-04-21 20:28:33 -0600

    Got vue output from apollo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- Apollo watched Graphql query -->
+    <ApolloQuery :query="require('./graphql/allArmour.gql')">
+      <template slot-scope="{ result: { loading, error, data } }">
+        <!-- Loading -->
+        <div v-if="loading" class="loading apollo">Loading...</div>
+
+        <!-- Error -->
+        <div v-else-if="error" class="error apollo">An error occured</div>
+
+        <!-- Result -->
+        <div v-else-if="data" class="result apollo">{{ data.AllArmour }}</div>
+
+        <!-- No result -->
+        <div v-else class="no-result apollo">No result :(</div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
