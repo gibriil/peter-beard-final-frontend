c22f6eb52d05433534c061db61ae41c9657fcc52
Gibriil <musicmandanceman@yahoo.com>
2020-04-21 18:14:47 -0600

    addeded apollo and bootstrap

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {
+  BootstrapVue,
+  IconsPlugin
+} from 'bootstrap-vue'
+import {
+  createProvider
+} from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
+  BootstrapVue,
+  IconsPlugin,
   render: h => h(App)
 }).$mount('#app')
