8c2bca57f31a537feeaffb09f571b6ba89563591
Gibriil <musicmandanceman@yahoo.com>
2020-04-30 00:50:15 -0600

    Added footer

--- src/components/wrapper/Footer.vue
+++ src/components/wrapper/Footer.vue
@@ -1,3 +1,9 @@
 <template>
-  <footer></footer>
+  <footer class="bg-dark">
+    <!-- Copyright -->
+    <div class="text-center py-3 mt-5">
+      <a class="text-white" href="https://github.com/gibriil">View my GitHub</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
 </template>
