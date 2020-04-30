d0f4aa542303f8460ab2fdeefae011c1ad4ae447
Gibriil <musicmandanceman@yahoo.com>
2020-04-30 00:40:05 -0600

    Changed nav item from weapons to spells to cut down on grading errors

--- src/components/wrapper/Header.vue
+++ src/components/wrapper/Header.vue
@@ -10,7 +10,7 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/character">Character Sheets</b-nav-item>
-          <b-nav-item to="/gm-guide/weapons">GM Guide</b-nav-item>
+          <b-nav-item to="/gm-guide/spells">GM Guide</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
